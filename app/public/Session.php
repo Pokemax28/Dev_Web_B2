c4e7007b4dabe6d89ba13a9271dd6e8fe58b9dff
Pokemax28 <maxime.cornu@edu.ece.fr>
2024-02-06 11:31:04 +0100

    debut de modif session

---
+++ app/public/Session.php
@@ -0,0 +1,30 @@
+<?php 
+
+class Session
+{
+    fonction__construct()
+    {
+        session_start();
+    }
+
+    public fonction add(string $key, $data)
+    {
+        $_SESSION[$key] = $data;
+    }
+
+    public fonction get(string $key)
+    {
+        return $_SESSION[$key];
+    }
+
+    public fonction destroy(string $key)
+    {
+        unset($_SESSION);
+        session_destroy();
+    }
+
+    public fonction isConnected()
+    {
+        return isset($_SESSION['user']);
+    }
+}
